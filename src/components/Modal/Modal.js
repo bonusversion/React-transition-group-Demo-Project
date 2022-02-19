6910e1fc9e424f04e5b5dc3d01a3d83bf0f772d6
mybonus <506832550@qq.com>
2022-02-19 12:56:19 +0800

    Animation Timings, Transition Events

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,23 +1,44 @@
 import React from "react";
-
+import Transition from "react-transition-group/cjs/Transition";
 import "./Modal.css";
 
 const modal = (props) => {
-  const cssClasses = [
-    "Modal",
-    props.show === "entering"
-      ? "ModalOpen"
-      : props.show === "exiting"
-      ? "ModalClosed"
-      : null,
-  ];
+  const animationTiming = {
+    enter: 400,
+    exit: 1000,
+  };
   return (
-    <div className={cssClasses.join(" ")}>
-      <h1>A Modal</h1>
-      <button className="Button" onClick={props.closed}>
-        Dismiss
-      </button>
-    </div>
+    <Transition
+      mountOnEnter
+      unmountOnExit
+      timeout={animationTiming}
+      in={props.show}
+      onEnter={() => console.log("onEnter")}
+      onEntering={() => console.log("onEntering")}
+      onEntered={() => console.log("onEntered")}
+      onExit={() => console.log("onExit")}
+      onExiting={() => console.log("onExiting")}
+      onExited={() => console.log("onExited")}
+    >
+      {(state) => {
+        const cssClasses = [
+          "Modal",
+          state === "entering"
+            ? "ModalOpen"
+            : state === "exiting"
+            ? "ModalClosed"
+            : null,
+        ];
+        return (
+          <div className={cssClasses.join(" ")}>
+            <h1>A Modal</h1>
+            <button className="Button" onClick={props.closed}>
+              Dismiss
+            </button>
+          </div>
+        );
+      }}
+    </Transition>
   );
 };
 export default modal;
